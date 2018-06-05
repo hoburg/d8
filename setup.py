030421ea6573cf7506187a3aec55ff6f1df48d95
1ozturkbe <1ozturkbe@gmail.com>
2018-06-05 11:01:16 -0400

    Making setup.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'SPaircraft',
+	version = '0.0.0',
+	description = 'Signomial programming compatible models for commercial aircraft design',
+    url='https://github.com/convexengineering/SPaircraft',
+    author='Berk Ozturk, Martin York',
+    author_email='bozturk@mit.edu',
+    license='MIT',
+    packages=[])
+:
