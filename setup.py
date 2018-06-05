40e47beb9c5debf5ad41450754bb12ac4368c4ee
1ozturkbe <1ozturkbe@gmail.com>
2018-06-05 11:19:04 -0400

    Improved setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+"""Python setup script for SPaircraft"""
 from setuptools import setup
+
+description = """
+Signomial programming compatible models for commercial aircraft design. 
+Requires installations of `GPkit <https://github.com/convexengineering/gpkit>`_
+and `turbofan <https://github.com/convexengineering/turbofan>`_. 
+`Documentation <http://spaircraft.readthedocs.io/en/latest/>`_
+"""
 
 setup(name = 'SPaircraft',
 	version = '0.0.0',
-	description = 'Signomial programming compatible models for commercial aircraft design',
+	description = description,
     url='https://github.com/convexengineering/SPaircraft',
     author='Berk Ozturk, Martin York',
     author_email='bozturk@mit.edu',
     license='MIT',
-    packages=[])
-:
+    packages=[],
+    install_requires = ['turbofan', 'gpkit'])
